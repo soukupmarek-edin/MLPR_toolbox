7cfff2881a723ae4592eb012d65c1790086c7ec8
SOUKUP Marek <s2091069@ed.ac.uk>
2020-10-27 03:05:15 +0000

    sigmoid added

--- functions.py
+++ functions.py
@@ -3,8 +3,14 @@ Available functions:
 
 - radial_basis_function_1d
 - sigmoid_1d
+- sigmoid
 - square_error_gradient
+
+LOSSES:
 - square_loss
+- cross_entropy_loss
+
+KERNELS:
 - linreg_kernel
 - gaussian_kernel
 - kernel_Sigma
@@ -18,11 +24,20 @@ def radial_basis_function_1d(x, c, h):
 def sigmoid_1d(x, v, b):
     return 1/(1+np.exp(-v*x-b))
 
+def sigmoid(a):
+    return 1/(1+np.exp(-a))
+
 def square_error_gradient(Phi, y, w):
     return -2*Phi.T@(y-Phi@w)
 
 def square_loss(Phi, y, w):
     return (y-Phi@w)**2
+
+def cross_entropy_loss(Phi, y, w):
+    y = y.flatten()
+    preds = sigmoid(Phi@w).flatten()
+    cost = -y*np.log(preds)-(1-y)*np.log(1-preds)
+    return cost.sum()/y.shape[0]
 
 def linreg_kernel(x1, x2, sigma_sq_w, sigma_sq_b):
     return sigma_sq_w*x1*x2 + sigma_sq_b
